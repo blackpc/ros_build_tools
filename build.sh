50643b6bbc096f91d86b43a0b7797aef64ee9ad4
Igor Makhtes <blackpc.me@gmail.com>
2013-12-04 12:03:20 +0200

    Better output format

--- build.sh
+++ build.sh
@@ -12,38 +12,73 @@ function cecho {
     echo -e '\E[37;44m'"\033[1m$1\033[0m"
 }
 
+function checked_run {
+    cecho "┌── $1"
+    shift
+
+    $* > /dev/null
+    return_value=$?
+
+    if [[ $return_value != 0 ]]; then
+        recho "└─╼ Unexpected error"
+        exit 1
+    else
+        gecho "└─╼ Successfuly done"        
+    fi
+    echo
+}
+
+function run {
+    cecho "┌── $1"
+    redirect_all_output=$2
+    shift
+    shift
+    if [[ $redirect_all_output = 'true' ]]; then
+        $* &> /dev/null
+    else
+        $*
+    fi
+    gecho "└─╼ Done"
+    echo
+}
+
 if [[ $# = 0 ]]; then
     cecho "Usage: $0 workspace-name repository-url"
     exit
 fi
 
-workspace=/home/cogni/workspaces/$1
+
+workspace=~/workspaces/$1
 repository=$2
 
-cecho "===================================================="
+cecho "==============================================================="
 cecho "Repository: $repository"
 cecho "Workspace : $workspace"
-cecho "===================================================="
+cecho "==============================================================="
 cecho "@ Creating catkin workspace..."
-rm $workspace -rf
-cecho " - Creating workspace directory..."
-mkdir $workspace -p
-cecho " - Initializing workspace..."
+echo
+
+checked_run "asd asd" echo 'hello world'
+
+run "Removing workspace if already exitst" true rm $workspace -rf
+checked_run "Creating workspace directory..." mkdir $workspace -p
+
 cd $workspace
 mkdir src
 cd src
-catkin_init_workspace
-cecho " - Compiling empty workspace..."
+checked_run "Initializing workspace..." catkin_init_workspace
+
 cd ..
-catkin_make > /dev/null && (gecho " - Successfuly done") || (recho " - Unexpected error" && exit)
-cecho " - Sourcing setup.bash..."
-source devel/setup.bash
-cecho " - Cloning repository..."
+checked_run "Compiling empty workspace..." catkin_make
+
+run "Sourcing setup.bash..." true source devel/setup.bash
+
 cd src
-git clone $repository > /dev/null
+checked_run "Cloning repository..." git clone $repository
+
 cd ..
-cecho " - Building workspace..."
-catkin_make > /dev/null && (gecho " - Successfuly done") || (recho " - Unexpected error" && exit)
-cecho " - Installing workspace..."
-catkin_make install > /dev/null && (gecho " - Successfuly done") || (recho " - Unexpected error" && exit)
+checked_run "Building workspace..." catkin_make
+
+checked_run "Installing workspace..." catkin_make install
+
 cecho "@ Done"
